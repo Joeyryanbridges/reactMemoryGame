0789b27f3ca8be040a6ade135aca4ed08f4b6e75
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-06-07 12:53:51 -0700

    Working on jumbotron css modules working

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -6,7 +6,6 @@ const Jumbotron = () => (
     <div className="container">
         <div className="jumbotron">
             <h1><center>React.js Memory Game</center></h1>
-            <center><h4>Rules:</h4></center>
         </div>
     </div>
 );
