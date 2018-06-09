67298b011c4b696c091d4fd26edecd01b2636182
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-06-08 21:27:24 -0700

    ColorCard.js set with an onclick, set on click function in app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 
 
+class App extends component {
+    state = {
+
+    }
+};
 
 <div>
     <Navbar />
     <Jumbotron />
 </div>
+
 
 export default App;
 
--- src/components/ColorCard/ColorCard.js
+++ src/components/ColorCard/ColorCard.js
@@ -2,19 +2,11 @@ import React from "react";
 import "./ColorCard.css";
 
 const ColorCard = props => (
-    <div className="card">
+    <div onClick={() => props.setClicked(props.id)} className="card">
         <div className="img-container">
             <img alt={props.name} src={props.image} />
         </div>
-        <div className="content">
-            <ul>
-                <li>
-                    <strong>Name:</strong> {props.name}
-                </li>
-            </ul>
-        </div>
     </div>
-
 );
 
 export default ColorCard;
