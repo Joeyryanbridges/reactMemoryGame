35f8e6846bfe793b07731ebfe7f21480a1024d5b
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-06-08 23:17:16 -0700

    adding additional components, still getting errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
+import Wrapper from "./components/Title";
+import Title from "./components/Wrapper";
+import colors from "./colors.json";
+
+
 
 
 class App extends component {
     state = {
-
-    }
+        colors,
+        correctGuesses,
+        bestScore,
+        clickMessage
+    };
+    // Setting this.state.matches to the 
 };
 
 <div>
