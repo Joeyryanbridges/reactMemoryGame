574400ece62fdbcd5dee37ca4d49fd6a2a851a0f
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-06-12 00:46:12 -0700

    Adding Animate.css to this party

--- src/components/ColorCard/ColorCard.js
+++ src/components/ColorCard/ColorCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./ColorCard.css";
 
 const ColorCard = props => (
-    <img src={props.image} id={props.id} onClick={props.shuffleCards} alt="cardColors" />
+    <img src={props.image} id={props.id} onClick={props.shuffleCards} alt="cardColors" className="animated infinite bounce" />
 );
 
 export default ColorCard;
